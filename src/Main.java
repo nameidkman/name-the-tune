733f39a8743c5e6756771bf7d082ddb29f55ed22
name_idk <Saianshbhatia77@gmail.com>
2025-03-10 22:50:52 -0400

    did a lot of things

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import javax.sound.sampled.*;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 
 public class Main {
@@ -14,27 +18,41 @@ public class Main {
     static final int numberOfPopSongs = 15;
     static final int numberOfJpopSongs = 15;
     static String[][] songDataSet = {
-            {"Bohemian Rhapsody", "Rock", "Path 1"},
-            {"Stairway to Heaven", "Rock", "Path 2"},
-            {"Hotel California", "Rock", "Path 3"},
-            {"Imagine", "Rock", "Path 4"},
-            {"Smells Like Teen Spirit", "Rock", "Path 5"},
-            {"Hey Jude", "Rock", "Path 6"},
-            {"Sweet Child O' Mine", "Rock", "Path 7"},
-            {"Comfortably Numb", "Rock", "Path 8"},
-            {"Another Brick in the Wall", "Rock", "Path 9"},
-            {"Dream On", "Rock", "Path 10"},
-            {"Let It Be", "Rock", "Path 11"},
-            {"Purple Haze", "Rock", "Path 12"},
-            {"Don't Stop Believin'", "Rock", "Path 13"},
-            {"Wish You Were Here", "Rock", "Path 14"},
-            {"Back In Black", "Rock", "Path 15"},
+            {"Bohemian Rhapsody", "Rock", "1.wav"},
+            {"Stairway to Heaven", "Rock", "2.wav"},
+            {"Hotel California", "Rock", "3.wav"},
+            {"Imagine", "Rock", "4.wav"},
+            {"Smells Like Teen Spirit", "Rock", "5.wav"},
+            {"Hey Jude", "Rock", "6.wav"},
+            {"Sweet Child O' Mine", "Rock", "7.wav"},
+            {"Comfortably Numb", "Rock", "8.wav"},
+            {"Another Brick in the Wall", "Rock", "9.wav"},
+            {"Dream On", "Rock", "10.wav"},
+            {"Let It Be", "Rock", "11.wav"},
+            {"Purple Haze", "Rock", "12.wav"},
+            {"Don't Stop Believin'", "Rock", "13.wav"},
+            {"Wish You Were Here", "Rock", "14.wav"},
+            {"Back In Black", "Rock", "15.wav"},
+            {"Upside Down" , "Pop", "16.wav"},
+            {"Come and Get your Love", "Pop", "17.wav"},
+            {"End of Beginning", "Pop", "18.wav"},
+            {"Shut Up and Dance with me", "Pop", "19.wav"},
+            {"Riptide", "Pop", "20.wav"},
+            {"Classic", "Pop", "21.wav"},
+            {"Deja Vu", "Pop", "22.wav"},
+            {"wake Me Up Before You Go-GO", "Pop", "23.wav"},
+            {"Ho Hey", "Pop", "24.wav"},
+            {"Everybody Want to Rule The world", "Pop", "25.wav"},
+            {"Make your own kind of music", "Pop", "26.wav"},
+            {"From the start", "Pop", "27.wav"},
+            {"I'm Gonna Be(500 Miles)", "Pop", "28.wav"},
+            {"Our Love", "Pop", "29.wav"},
+            {"I Like me Better", "Pop", "30.wav"},
     };
 
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws InterruptedException, IOException, LineUnavailableException, UnsupportedAudioFileException {
         /*
-
-            the main things which i need to do before this
+            Done -
             - check which is the genre the user wants to do
             - make a timer for the user for 30 second
             - stop it every single time the user enter somehting
@@ -42,7 +60,6 @@ public class Main {
             - this will give them 4 option to chose from and they will need to
                 and if they get the right answer then that is a good thing
             - make a menu for hte user
-
         */
 
         String sgenre = "";
@@ -60,32 +77,36 @@ public class Main {
         System.out.println("the genres are - ");
         System.out.println("1 - ROCK");
         System.out.println("2 - POP MUSIC");
-        System.out.println("4 - JPON/ANIME");
+        System.out.println("3 - JPON/ANIME");
         sgenre = in.nextLine();
-        genre = inputCheck(sgenre, 1, 5);
-
+        genre = inputCheck(sgenre, 1, 3);
         // creating a switch case for the genre which the user has entered
         switch (genre) {
-            case 1 -> rock();
-            case 2 -> popMusic();
-            case 3 -> country();
-            case 4 -> jpon();
-            case 5 -> jazz();
-            default ->
+            case 1:
+                rock();
+                break;
+            case 2:
+                popMusic();
+                break;
+            case 3:
+                jpon();
+                break;
+            default:
                     System.out.println("i dont think this is possible as there is a for while loop which is just made for this ");
+                break;
         }
 
         in.close();
     }
 
     // if the user wants to rock
-    static void rock() throws IOException, InterruptedException {
+    static void rock() throws IOException, InterruptedException, UnsupportedAudioFileException, LineUnavailableException {
 
         // this is for the name, genre path
         // the reason why i do it this was is because it is a lot more consize  for now
         Songs[] songs = new Songs[numberOfRockSongs];
         int answer = 1, res = 0, uAns = 0;
-        int[] songIndexes = shuffle(1, 14, 7);
+        int[] songIndexes = shuffle(0, 14, 7);
         String a = "";
 
 
@@ -98,7 +119,12 @@ public class Main {
             clearConsole();
             System.out.println("So lets see how many can you get right");
             System.out.println("GUESS THE TUNE");
-            timer = timer(timer);
+
+            for (int j = 0; j < songIndexes.length; j++)
+                System.out.print(songIndexes[j] + " ");
+
+
+            timer = timer(timer, songs, songIndexes, i);
             if (timer <= 0) {
                 break;
             }
@@ -120,34 +146,118 @@ public class Main {
         System.out.println("lets goooooo");
     }
 
-    static void popMusic() {
+    static void popMusic() throws UnsupportedAudioFileException, LineUnavailableException, IOException, InterruptedException {
+        // this is for the name, genre path
+        // the reason why i do it this was is because it is a lot more consize  for now
+        Songs[] songs = new Songs[numberOfPopSongs];
+        int answer = 1, res = 0, uAns = 0;
+        int[] songIndexes = shuffle(0, 14, 7);
+        String a = "";
+
+
+        for (int i = numberOfRockSongs; i < numberOfRockSongs +  numberOfPopSongs; i++) {
+            if (songDataSet[i][1].equals("Pop"))
+                songs[i] = new Songs(songDataSet[i][0], songDataSet[i][1], songDataSet[i][2]);
+        }
+
+        for (int i = 0; i < 7; i++) {
+            clearConsole();
+            System.out.println("So lets see how many can you get right");
+            System.out.println("GUESS THE TUNE");
+
+            for (int j = 0; j < songIndexes.length; j++)
+                System.out.print(songIndexes[j] + " ");
+
+
+            timer = timer(timer, songs, songIndexes, i);
+            if (timer <= 0) {
+                break;
+            }
+            System.out.println("So I am assuming you know that fine tune");
+            System.out.println("So here are your options");
+            answer = option(songs, songIndexes, i);
+            a = in.nextLine();
+            uAns = inputCheck(a, 1, 4);
+            if (answer == uAns) {
+                res++;
+                System.out.println("you got it right nice job");
+            } else {
+                System.out.println("no you got it wrong dont worry");
+                System.out.println("you will get the next one right");
+            }
+        }
+        clearConsole();
+        System.out.println("you in the end got " + res + " right");
+        System.out.println("lets goooooo");
 
     }
 
-    static void country() {
+    static void jpon() throws UnsupportedAudioFileException, LineUnavailableException, IOException, InterruptedException {
+        Songs[] songs = new Songs[numberOfRockSongs];
+        int answer = 1, res = 0, uAns = 0;
+        int[] songIndexes = shuffle(0, 14, 7);
+        String a = "";
+
+
+        for (int i = numberOfRockSongs + numberOfPopSongs; i < numberOfJpopSongs + numberOfPopSongs + numberOfRockSongs; i++) {
+            if (songDataSet[i][1].equals("JPop"))
+                songs[i] = new Songs(songDataSet[i][0], songDataSet[i][1], songDataSet[i][2]);
+        }
+
+        for (int i = 0; i < 7; i++) {
+            clearConsole();
+            System.out.println("So lets see how many can you get right");
+            System.out.println("GUESS THE TUNE");
+
+            for (int j = 0; j < songIndexes.length; j++)
+                System.out.print(songIndexes[j] + " ");
+
+
+            timer = timer(timer, songs, songIndexes, i);
+            if (timer <= 0) {
+                break;
+            }
+            System.out.println("So I am assuming you know that fine tune");
+            System.out.println("So here are your options");
+            answer = option(songs, songIndexes, i);
+            a = in.nextLine();
+            uAns = inputCheck(a, 1, 4);
+            if (answer == uAns) {
+                res++;
+                System.out.println("you got it right nice job");
+            } else {
+                System.out.println("no you got it wrong dont worry");
+                System.out.println("you will get the next one right");
+            }
+        }
+        clearConsole();
+        System.out.println("you in the end got " + res + " right");
+        System.out.println("lets goooooo");
 
     }
 
-    static void jpon() {
-
-    }
-
-    static void jazz() {
-    }
 
     // check the times which the user has used
-    static int timer(int remainTime) throws InterruptedException, IOException {
+    static int timer(int remainTime, Songs[] songs, int[] songIndexes, int currentIndex) throws InterruptedException, IOException, UnsupportedAudioFileException, LineUnavailableException {
         String check = "";
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        File file = new File(songs[songIndexes[currentIndex]].path);
+        System.out.println(file.getAbsolutePath());
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
 
         while (remainTime > 0) {
+            clip.start();
             if (reader.ready()) {
                 check = String.valueOf(reader.read());
                 if (check != null) {
                     if (check.charAt(0) == 32) {
+                        clip.stop();
                         break;
                     } else {
                         remainTime++;
+                        clip.stop();
                         break;
                     }
                 }
@@ -203,7 +313,6 @@ public class Main {
                 System.out.println("To break the time what you should do is press the enter");
                 System.out.println("And the most important thing is to have fun");
                 System.out.println("lets goo");
-                clearConsole();
 
             } else if (input.equals("2")) {
                 break;
@@ -272,47 +381,60 @@ public class Main {
 
     }
 
-    public static int option(Songs[] song, int[] num, int currentIndex) {
+    public static int option(Songs[] song, int[] songIndexes, int currentIndex) {
 
-        int[] a = shuffle(1, 14, 3);
+        int[] a = new int[3];
+        for (int i = 0; i < songIndexes.length; i++) {
+            do {
+                a = shuffle(0, 14, 3);
+            } while (containsOverlap(a, songIndexes));
+        }
+
         int rand = (int) (Math.random() * 4) + 1;
-
         switch (rand) {
             case 1:
-                System.out.println(song[currentIndex].name);
-                printSongs(song, a, currentIndex);
+                System.out.println(song[songIndexes[currentIndex]].name);
+                System.out.println(song[a[0]].name);
+                System.out.println(song[a[1]].name);
+                System.out.println(song[a[2]].name);
                 break;
             case 2:
-                printSongs(song, a, currentIndex);
-                System.out.println(song[currentIndex].name);
+                System.out.println(song[a[0]].name);
+                System.out.println(song[songIndexes[currentIndex]].name);
+                System.out.println(song[a[1]].name);
+                System.out.println(song[a[2]].name);
                 break;
             case 3:
-                printSongs(song, a, currentIndex);
-                System.out.println(song[currentIndex].name);
+                System.out.println(song[a[0]].name);
+                System.out.println(song[a[1]].name);
+                System.out.println(song[songIndexes[currentIndex]].name);
+                System.out.println(song[a[2]].name);
                 break;
             default:
-                printSongs(song, a, currentIndex);
-                System.out.println(song[currentIndex].name);
+                System.out.println(song[a[0]].name);
+                System.out.println(song[a[1]].name);
+                System.out.println(song[a[2]].name);
+                System.out.println(song[songIndexes[currentIndex]].name);
                 break;
         }
+
 
         System.out.println(rand);
         return rand;
     }
 
-
-    private static void printSongs(Songs[] song, int[] a, int currentIndex) {
-        for (int i : a) {
-            if (i == currentIndex) {
-                if (currentIndex != 0) {
-                    System.out.println(song[currentIndex - 1].name);
-                } else {
-                    System.out.println(song[currentIndex + 1].name);
-                }
-            } else {
-                System.out.println(song[i].name);
-            }
-
+    public static boolean containsOverlap(int[] a, int[] songIndexes) {
+        Set<Integer> songSet = new HashSet<>();
+        for (int song : songIndexes) {
+            songSet.add(song);
         }
+        for (int num : a) {
+            if (songSet.contains(num)) {
+                return true; // There's an overlap
+            }
+        }
+        return false;
     }
+
+
 }
